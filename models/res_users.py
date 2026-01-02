ec1eb133e4a8026a035915ca21b62a8b0702d334
muhammadk1607 <muhammadkhan1607@gmail.com>
2026-01-02 14:04:10 +0500

    Check overwrite signup

--- models/res_users.py
+++ models/res_users.py
@@ -45,3 +45,42 @@ class ResUsers(models.Model):
         _logger.info("Created internal user %s", values["login"])
 
         return values
+
+    @api.model
+    def _signup_create_user(self, values):
+        """signup a new user using the template user"""
+
+        email = values.get("email")
+
+        if not email or not email.endswith("@10xengineers.ai"):
+            # Not a 10xengineers.ai email, return
+            return values
+
+        # Try to find existing partner by email
+        partner = self.env["res.partner"].search([("email", "=", email)], limit=1)
+
+        if partner:
+            _logger.info("Found existing partner for %s", email)
+            # Partner exists, use it
+            values["partner_id"] = partner.id
+        else:
+            _logger.info("Creating new partner for %s", email)
+            # Create new partner
+            partner = self.env["res.partner"].create(
+                {
+                    "name": values.get("name"),
+                    "email": email,
+                    "company_type": "person",
+                }
+            )
+            values["partner_id"] = partner.id
+
+        # Make internal user
+        internal_group = self.env.ref("base.group_user")
+        values["groups_id"] = [(6, 0, [internal_group.id])]
+
+        _logger.info("Created internal user %s", values["login"])
+
+        self.env["res.users"].create(values)
+
+        partner.write({"user_id": values["id"]})
