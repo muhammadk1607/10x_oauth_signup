93299d7f43e008413bae4f2ce11e67cf46ffd2d4
muhammadk1607 <muhammadkhan1607@gmail.com>
2026-01-02 14:18:37 +0500

    🔥 Remove extra

--- models/res_users.py
+++ models/res_users.py
@@ -9,44 +9,6 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     @api.model
-    def _generate_signup_values(self, provider, validation, params):
-        """Override to find/create partner and set as internal user"""
-        values = super()._generate_signup_values(provider, validation, params)
-
-        email = validation.get("email") or params.get("email")
-
-        if not email or not email.endswith("@10xengineers.ai"):
-            # Not a 10xengineers.ai email, return
-            return values
-
-        # Try to find existing partner by email
-        partner = self.env["res.partner"].search([("email", "=", email)], limit=1)
-
-        if partner:
-            _logger.info("Found existing partner for %s", email)
-            # Partner exists, use it
-            values["partner_id"] = partner.id
-        else:
-            _logger.info("Creating new partner for %s", email)
-            # Create new partner
-            partner = self.env["res.partner"].create(
-                {
-                    "name": validation.get("name"),
-                    "email": email,
-                    "company_type": "person",
-                }
-            )
-            values["partner_id"] = partner.id
-
-        # Make internal user
-        internal_group = self.env.ref("base.group_user")
-        values["groups_id"] = [(6, 0, [internal_group.id])]
-
-        _logger.info("Created internal user %s", values["login"])
-
-        return values
-
-    @api.model
     def _signup_create_user(self, values):
         """signup a new user using the template user"""
 
@@ -79,4 +41,8 @@ class ResUsers(models.Model):
 
         self.env["res.users"].create(values)
 
+        _logger.info("Created internal user %s", values["login"])
+
         partner.write({"user_id": values["id"]})
+
+        _logger.info("Set partner user_id to %s", values["login"])
